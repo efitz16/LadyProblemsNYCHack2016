9c3ee674a26400425afa5f1dae81fa689b779d7f
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-10-07 19:17:44 -0400

    seeds completed

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,4 @@
 class EntriesController < ApplicationController
+	def show
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'entries#index' #delete this
+  resources :entries, :only => [:show, :index]
   resources :searches, :only => [:show, :index]
 end

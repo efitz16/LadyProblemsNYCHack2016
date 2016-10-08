85adec5dd56248a81919a4e978c5017b747afb57
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-10-07 23:25:08 -0400

    fix routes merge conflict

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,11 @@
 class SearchesController < ApplicationController
+	def index
+		binding.pry
+	end
 end
+
+ 
+# put this in view where want to search
+  	# <div class="form-group" id="search-form">
+   #    <%= render 'searches/search' %>
+   #  </div>
--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ resources :entries, except: [:show]
 
 resources :entries, only: [:show] do
   resources :bills, except: [:index]
-end
 
 resources :bills, only: [:show] do
   resources :items, except: [:index, :show]
 end
 
+resources :searches, :only => [:show, :index]
 end

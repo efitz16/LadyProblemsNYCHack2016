e0b182305d9dc0ca8ad9af59b1fbb51ca48ba266
caitlinchai <caitlinchai.2012@gmail.com>
2016-10-08 02:36:50 -0400

    add user registration, authentication, sessions, and respective routes and validations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
+  root 'search#index'
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+  resources :users, except: [:index]
+  resources :entries, except: [:show]
+  resources :entries, only: [:show] do
+    resources :bills, except: [:index]
+  end
 
-root 'search#index'
-
-resources :users, except: [:index]
-
-resources :entries, except: [:show]
-
-resources :entries, only: [:show] do
-  resources :bills, except: [:index]
-end
-
-resources :bills, only: [:show] do
-  resources :items, except: [:index, :show]
-end
+  resources :bills, only: [:show] do
+    resources :items, except: [:index, :show]
+  end
 
 end

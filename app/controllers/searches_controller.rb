f5d887ee101128535dce1ebcb5466275bc917da4
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-10-08 23:40:26 -0400

    create distance custom search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,38 +1,65 @@
 class SearchesController < ApplicationController
-	def index
-	end
+  def index
+  end
 
-	def new
-		binding.pry
-		@results = Entry.search(params["search"])
-		@results = @results.paginate(:page => params[:page], :per_page => 6)
+  def new
+  	@results = Entry.search(params["search"])
+  	@results = @results.paginate(:page => params[:page], :per_page => 6)
 		
-		if !(@results.empty?)
-			@min_max = @results.map { |result| result.total_cost }
-		end
+	if !(@results.empty?)
+	  @min_max = @results.map { |result| result.total_cost }
+	  @min_max = @min_max.minmax
+	end
 
 	end
 
-	def data
-		respond_to do |format|
-			format.json {
-				render :json => [1,2,3,4,5]
-			}
-		end
+  def data
+    respond_to do |format|
+    	format.json {
+    		render :json => [1,2,3,4,5]
+    	}
+    end
+  end
+
+  def customize
+    @results = Entry.search(params["query"])
+
+	@new_results = []
+
+	if params[:insurance]
+	  @results.each do |result| 
+	  	@new_results << result.bills.where(insurance_company: current_user.insurance_company)
+	  	@new_results = @new_results.flatten
+	  end
 	end
 
-	def customize
-		binding.pry
-		@results = Entry.search(params["query"])
+	if params[:price]
+	  if !@new_results.empty?
 
-		if params[:insurance]
-			Entry.search_insurance(@results)
-		elsif params[:price]
+	  else
 
-		elsif params[:distance]
+	  end
+	end
+
+	if params[:distance]
+			# binding.pry
+      if !@new_results.empty?
+        @results1 = @new_results
+        @new_results = @new_results.select { |result| result.city = current_user.city }
+        @new_results << @results1.select { |result| result.state = current_user.state && !(@new_results.include?(result)) }
+	  else
+		@new_results = @results.select { |result| result.city = current_user.city }
+		@new_results << @results.select { |result| result.state = current_user.state && !(@new_results.include?(result)) }
+	  end
+
+	  @new_results = @new_results.flatten
+	end
+
+	@results = @new_results
+	@results = @results.paginate(:page => params[:page], :per_page => 6)
+
+	render 'new'
 			
-		end
+  end
 
-
-	end
 end
